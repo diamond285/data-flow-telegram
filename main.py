a3cef381b9cfc066e34696e02bb24747943191dd
diamond285 <59549446+diamond285@users.noreply.github.com>
2023-06-14 04:29:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -17,22 +17,19 @@ authes = {}
 @bot.message_handler(content_types=['text'])
 def start(message):
     request_code = hashlib.md5(
-        str(datetime.now(pytz.timezone('GMT')).replace(second=0, microsecond=0)).split('+')[0].encode('utf-8')
+        str(datetime.now(pytz.timezone('Asia/Almaty')).replace(second=0, microsecond=0)).split('+')[0].encode('utf-8')
     ).hexdigest()[-5:]
     req = requests.get(f'http://docs-flow.ru/getUser.php?request_code={request_code}&user={message.text}').text
+    print(req)
     try:
         x = json.loads(req)
         if x:
             code = hashlib.md5(
                 (str(
-                    datetime.now(pytz.timezone('GMT')).replace(second=0, microsecond=0)
+                    datetime.now(pytz.timezone('Asia/Almaty')).replace(second=0, microsecond=0)
                 ).split('+')[0]
                  + x['username']).encode('utf-8')
             ).hexdigest()[-5:]
-            bot.send_message(message.from_user.id, str(
-                datetime.now(pytz.timezone('GMT')).replace(second=0, microsecond=0)
-            ).split('+')[0]
-            + x['username'])
             bot.send_message(message.from_user.id, f"Welcome, {x['fullname']}!\nYour code: {code}")
             authes[message.from_user.id] = x['fullname']
     except:
